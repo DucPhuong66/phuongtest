bc7ce2886367d3f7d100cb4e4f72d6fc86cb3c20
Phuong Duc Doan <ducphuong200@gmail.com>
2024-01-11 13:54:28 +0700

    format code

--- js/script.js
+++ js/script.js
@@ -1,142 +1,105 @@
-// var swiper = new Swiper(".swiper", {
-//     autoplay: {
-//         delay: 2500,
-//         disableOnInteraction: false,
-//       },
-
-//       slidesPerView: 3,
-//       spaceBetween: 24,
-//       freeMode: true,
-//     navigation: {
-//       nextEl: ".swiper-button-next",
-//       prevEl: ".swiper-button-prev",
-//     },
-//   });
-
-//   $(window).on("scroll", function() {
-//     if($(window).scrollTop() > 50) {
-//         $("#header").addClass("active");
-//     } else {
-//         //remove the background property so it comes transparent again (defined in your css)
-//        $("#header").removeClass("active");
-//     }
-// });
-
 // Show the first tab and hide the rest PC
-$('.jobsTab li:first-child').addClass('on');
-$('.sliderContent').hide();
-$('.sliderContent:first').show();
+$(".jobsTab li:first-child").addClass("on");
+$(".sliderContent").hide();
+$(".sliderContent:first").show();
 
 // Click function PC
-$('.jobsTab li').click(function(){
-  $('.jobsTab li').removeClass('on');
-  $(this).addClass('on');
-  $('.sliderContent').hide();
-  
-  var activeTab = $(this).find('a').attr('href');
+$(".jobsTab li").click(function () {
+  $(".jobsTab li").removeClass("on");
+  $(this).addClass("on");
+  $(".sliderContent").hide();
+
+  var activeTab = $(this).find("a").attr("href");
   $(activeTab).fadeIn();
   return false;
 });
 
-$('#latestWorks .jobsTab li').click(function(){
-    $(this).parent().toggleClass('active')
-    $('.fa-angle-down').remove()
-    $(this).append('<i class="sp fa-solid fa-angle-down"></i>')
-   
-})
+$("#latestWorks .jobsTab li").click(function () {
+  $(this).parent().toggleClass("active");
+  $(".fa-angle-down").remove();
+  $(this).append('<i class="sp fa-solid fa-angle-down"></i>');
+});
 
 // Show the first tab and hide the rest MB
-$('.workList').hide();
-$('.workList:first').css('display', 'flex');
- 
+$(".workList").hide();
+$(".workList:first").css("display", "flex");
 
 // Click function MB
-$('.jobsTab li').click(function(){
-  $('.workList').hide();
-  var activeTab = $(this).find('a').attr('href');
-  $(activeTab+'.workList ').fadeIn();
+$(".jobsTab li").click(function () {
+  $(".workList").hide();
+  var activeTab = $(this).find("a").attr("href");
+  $(activeTab + ".workList ").fadeIn();
   return false;
 });
 
-$('.defaultLang').click(function(){
+$(".defaultLang").click(function () {
   $(this).parent().toggleClass("active");
-})
+});
 
-$('.langMb li').click(function () {
+$(".langMb li").click(function () {
   var current = $(this).html();
   $(".defaultLang").html(current);
 });
 
-$('.menuBtn').click(function () {
-  $('.menuBox').toggleClass("active");
-})
+$(".menuBtn").click(function () {
+  $(".menuBox").toggleClass("active");
+});
 
-$('.close').click(function () {
-  $('.menuBox').toggleClass("active");
-})
-
-
-
-
-
+$(".close").click(function () {
+  $(".menuBox").toggleClass("active");
+});
 
 //lang Btn
-$(function() {
-  var dd1 = new dropDown($('#myDropdown'));
-  
-  $(document).click(function() {
-    $('.wrapper-dropdown').removeClass('active');
+$(function () {
+  var dd1 = new dropDown($("#myDropdown"));
+
+  $(document).click(function () {
+    $(".wrapper-dropdown").removeClass("active");
   });
 });
 
 function dropDown(el) {
   this.dd = el;
-  this.placeholder = this.dd.children('span');
-  this.opts = this.dd.find('ul.dropdown > li');
-  this.val = '';
+  this.placeholder = this.dd.children("span");
+  this.opts = this.dd.find("ul.dropdown > li");
+  this.val = "";
   this.index = -1;
   this.initEvents();
 }
 dropDown.prototype = {
-  initEvents: function() {
+  initEvents: function () {
     var obj = this;
-    
-    obj.dd.on('click', function() {
-      $(this).toggleClass('active');
+
+    obj.dd.on("click", function () {
+      $(this).toggleClass("active");
       return false;
     });
-    
-    obj.opts.on('click', function() {
+
+    obj.opts.on("click", function () {
       var opt = $(this);
       obj.val = opt.text();
       obj.index = opt.index();
       obj.placeholder.text(obj.val);
     });
-  }
-}
- 
+  },
+};
 
-
-let width = $(window).width() 
+let width = $(window).width();
 if (width > 768) {
   var swiper = new Swiper(".swiper", {
     autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
+      delay: 2500,
+      disableOnInteraction: false,
+    },
 
-      slidesPerView: 3,
-      spaceBetween: 24,
-      freeMode: true,
+    slidesPerView: 3,
+    spaceBetween: 24,
+    freeMode: true,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
   });
 } else {
-  $('.overlay .name br').remove()
+  $(".overlay .name br").remove();
 }
-
-
-
-
